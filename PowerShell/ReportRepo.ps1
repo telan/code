c2fda4b0d3c99a2b7bca2b090b831bc1cb07a338
telan <3791328+telan@users.noreply.github.com>
2023-11-18 19:50:15 -0500

    update

--- PowerShell/ReportRepo.ps1
+++ PowerShell/ReportRepo.ps1
@@ -1,15 +1,12 @@
 
-$wcitRepo = ""
-$wcitSubj = ""
+$wcitRepo = "wcit"
+$wcitSubj = "drivers"
 
 $wcitRepoPath = "c:\"+$wcitRepo+"\"
 $wcitSubjPath = $wcitRepoPath+$wcitSubj+"\"
 $wcitReport = $wcitSubj+".txt"
 $wcitReportPath = $wcitSubjPath+$wcitReport
 
-$wcitRepo
-$wcitRepoPath
-$wcitSubj
-$wcitSubjPath
-$wcitReport
-$wcitReportPath
+if (!(Test-Path $wcitRepo)) {New-Item -ItemType Directory -Path $wcitRepo}
+if (!(Test-Path $wcitSubj)) {New-Item -ItemType Directory -Path $wcitSubj}
+New-Item -Force -ItemType "file" -Name $wcitReport -Path $wcitSubjPath
