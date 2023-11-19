7130f4f25a9f376cab57f3b43ba064feb5a0af91
telan <3791328+telan@users.noreply.github.com>
2023-11-18 20:36:08 -0500

    update

--- PowerShell/winget-system.ps1
+++ PowerShell/winget-system.ps1
@@ -5,6 +5,6 @@ $wingetPath = Get-ChildItem "winget.exe" -Path 'C:\Program Files\WindowsApps\Mic
 
 & $wingetPath upgrade --accept-package-agreements --accept-source-agreements --exact --id= --silent
 
-& $wingetPath upgrade  --accept-package-agreements --accept-source-agreements --all --silent
+& $wingetPath upgrade --accept-package-agreements --accept-source-agreements --all --silent
 
 & $wingetPath uninstall --accept-source-agreements --exact --id= --silent
